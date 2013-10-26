080fc8a2728461e4d61ad295128698221c19f28e
Evan Bender <ebender@usnews.com>
2013-10-26 01:20:17 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Everything',
+      version='0.1',
+      description='Stock/Flow modeling in Python',
+      author='Evan Bender',
+      author_email='maccruiskeen@gmail.com',
+      packages=['everything'],
+      )
